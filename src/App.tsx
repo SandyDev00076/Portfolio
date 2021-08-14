27a113b19248ba07c467afa1c672db2d5c73b20f
Sanjeet <sanjeettiwari00076@gmail.com>
2021-08-15 02:25:15 +0530

    create intro in Home comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import Home from "components/Home";
+import Portfolio from "components/Portfolio";
 
 function App() {
-  const [homeFlag, showHome] = useState(false);
-  return <div></div>;
+  const [homeFlag, showHome] = useState(true);
+  if (homeFlag) return <Home />;
+  return <Portfolio />;
 }
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import styles from './Home.module.scss';
+import React from "react";
+import styles from "./Home.module.scss";
 
 const Home = () => {
-    return <section>
-        Home component
+  return (
+    <section className={styles.container}>
+      <section className={styles.intro}>
+        <h1>Sanjeet Tiwari</h1>
+        <div className={styles.role}>
+          <h2>UI/UX Designer</h2>
+          <h2>UI Developer</h2>
+        </div>
+      </section>
     </section>
-}
+  );
+};
 
 export default Home;
