9b0004fbb1faeae71066120e5b37a149f8e7d840
Sanjeet <sanjeettiwari00076@gmail.com>
2021-08-21 23:20:52 +0530

    include icons in the project

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,44 @@
+// Skill Icons
+import { ReactComponent as AngularIcon } from "./angular.svg";
+import { ReactComponent as FigmaIcon } from "./figma.svg";
+import { ReactComponent as ReactIcon } from "./react.svg";
+import { ReactComponent as VueIcon } from "./vuedotjs.svg";
+import { ReactComponent as VSCodeIcon } from "./visualstudiocode.svg";
+import { ReactComponent as SvelteIcon } from "./svelte.svg";
+
+// Social media icons
+import { ReactComponent as BehanceIcon } from "./behance.svg";
+import { ReactComponent as GithubIcon } from "./github.svg";
+import { ReactComponent as LinkedInIcon } from "./linkedin.svg";
+import { ReactComponent as PinterestIcon } from "./pinterest.svg";
+
+// Icons that represent design ideas
+import { ReactComponent as ArchIcon } from "./arch.svg";
+import { ReactComponent as BrushIcon } from "./brush.svg";
+
+// Icons that represent hybrid apps
+import { ReactComponent as LaptopIcon } from "./laptop.svg";
+import { ReactComponent as PhoneIcon } from "./phone.svg";
+
+// Icons that represent web dev
+import { ReactComponent as PublicIcon } from "./public.svg";
+import { ReactComponent as WebIcon } from "./web.svg";
+
+export {
+  AngularIcon,
+  FigmaIcon,
+  ReactIcon,
+  VueIcon,
+  VSCodeIcon,
+  SvelteIcon,
+  BehanceIcon,
+  GithubIcon,
+  LinkedInIcon,
+  PinterestIcon,
+  ArchIcon,
+  BrushIcon,
+  LaptopIcon,
+  PhoneIcon,
+  PublicIcon,
+  WebIcon,
+};
