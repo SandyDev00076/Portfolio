175ee9b8ff684c616c604d49b42d777b579c02ce
Sanjeet <sanjeettiwari00076@gmail.com>
2021-08-21 20:49:28 +0530

    add preClick and anim handler

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Button.module.scss";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = ({ children, ...props }: Props) => {
+const Button = ({ children, onClick, ...props }: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  function animateContainer() {
+    // TODO: put some container animation here
+  }
+
+  function preClick() {
+    animateContainer();
+  }
+
   return (
-    <div className={styles.container}>
-      <button {...props} className={styles.btn}>
+    <div className={styles.container} ref={containerRef}>
+      <button
+        {...props}
+        onClick={(e) => {
+          preClick();
+          if (onClick !== undefined) onClick(e);
+        }}
+        className={styles.btn}
+      >
         {children}
       </button>
       <div />
