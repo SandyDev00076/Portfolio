7473d19010651d6e52f29298d7587505c801b3fa
Sanjeet <sanjeettiwari00076@gmail.com>
2021-09-04 00:11:18 +0530

    finalize footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,54 @@ import React from "react";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={styles.container}>This is the footer</footer>;
+  return (
+    <footer className={styles.container}>
+      <h1>
+        Portfolio website created and designed by <strong>me</strong>.
+      </h1>
+      <h2>Hope you liked it!</h2>
+      <div className={styles.toolsUsed}>
+        <div>
+          Created using{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://reactjs.org/"
+          >
+            React
+          </a>{" "}
+          and{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.typescriptlang.org/"
+          >
+            TypeScript
+          </a>
+        </div>
+        <div>
+          Designed using{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.figma.com/"
+          >
+            Figma
+          </a>
+        </div>
+        <div>
+          Hosted using{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://vercel.com/"
+          >
+            Vercel
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
