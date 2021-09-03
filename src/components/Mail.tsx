7abb75006e406eb235efab2419a442e9f9b166be
Sanjeet <sanjeettiwari00076@gmail.com>
2021-09-03 20:05:42 +0530

    add intersection observer in mail section

--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -1,17 +1,35 @@
 import Button from "atoms/Button";
 import { MAILID } from "../constants";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import styles from "./Mail.module.scss";
 
 const Mail = () => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const options = {
+      root: null,
+      threshold: 0.6,
+      rootMargin: "0px",
+    };
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        contentRef.current?.classList.add(styles.appear);
+        if (contentRef.current) observer.unobserve(contentRef.current);
+      });
+    }, options);
+    if (contentRef.current) observer.observe(contentRef.current);
+  }, []);
   return (
     <section className={styles.container}>
-      <h1>Wanna reach out to me ?</h1>
-      <h2>Drop a mail</h2>
-      <a href={`mailto:${MAILID}`}>
-        <Button>Write a mail</Button>
-      </a>
+      <div className={styles.content} ref={contentRef}>
+        <h1>Wanna reach out to me ?</h1>
+        <h2>Drop a mail</h2>
+        <a href={`mailto:${MAILID}`}>
+          <Button>Write a mail</Button>
+        </a>
+      </div>
     </section>
   );
 };
