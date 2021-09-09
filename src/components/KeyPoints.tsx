70a36b81c9dd0cab5aebb5d5edd7e0c9eb757794
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-09-09 21:32:55 +0530

    add marquee animation

--- src/components/KeyPoints.tsx
+++ src/components/KeyPoints.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { BrushIcon, LaptopIcon, PublicIcon } from "assets/icons";
 import KeyPoint from "./KeyPoint";
 
 import styles from "./KeyPoints.module.scss";
 
+const CARD_ANIM_DURATION = 4000;
+
 const KeyPoints = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const cards = Array.from(containerRef.current.children);
+    function startAnim() {
+      cards.forEach((card, index) => {
+        setTimeout(() => {
+          card.classList.add(styles.animateSlide);
+          setTimeout(() => {
+            card.classList.remove(styles.animateSlide);
+          }, CARD_ANIM_DURATION);
+        }, index * CARD_ANIM_DURATION);
+      });
+    }
+    startAnim();
+    setInterval(() => {
+      startAnim();
+    }, cards.length * CARD_ANIM_DURATION);
+  }, []);
+
   return (
     <section className={styles.container}>
-      <div className={styles.keypoints}>
+      <div className={styles.keypoints} ref={containerRef}>
         <KeyPoint
           title="Web Apps"
           line="Created responsive web apps, PWAs"
@@ -22,11 +45,6 @@ const KeyPoints = () => {
           title="Designs"
           line="Created and published designs using Figma, Adobe XD and Behance"
           icon={<BrushIcon />}
-        />
-        <KeyPoint
-          title="Web Apps"
-          line="Created responsive web apps, PWAs"
-          icon={<PublicIcon />}
         />
       </div>
     </section>
