658e46254154b806be3ac8d0aa1145c426030ae0
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-09-13 00:14:34 +0530

    add links to mail section

--- src/components/Mail.tsx
+++ src/components/Mail.tsx
@@ -1,6 +1,18 @@
-import Button from "atoms/Button";
-import { MAILID } from "../constants";
 import React, { useEffect, useRef } from "react";
+import Button from "atoms/Button";
+import {
+  BEHANCE_URL,
+  GITHUB_URL,
+  LINKEDIN_URL,
+  MAILID,
+  PINTEREST_URL,
+} from "../constants";
+import {
+  BehanceIcon,
+  PinterestIcon,
+  LinkedInIcon,
+  GithubIcon,
+} from "assets/icons";
 
 import styles from "./Mail.module.scss";
 
@@ -32,9 +44,25 @@ const Mail = () => {
       <div className={styles.content} ref={contentRef}>
         <h1>Wanna reach out to me ?</h1>
         <h2>Get in touch</h2>
-        <a href={`mailto:${MAILID}`}>
-          <Button>Write a mail</Button>
-        </a>
+        <div className={styles.options}>
+          <a href={`mailto:${MAILID}`}>
+            <Button>Write a mail</Button>
+          </a>
+          <section className={styles.links}>
+            <a href={BEHANCE_URL} target="_blank" rel="noopener noreferrer">
+              <BehanceIcon />
+            </a>
+            <a href={PINTEREST_URL} target="_blank" rel="noopener noreferrer">
+              <PinterestIcon />
+            </a>
+            <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon />
+            </a>
+            <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
+              <GithubIcon />
+            </a>
+          </section>
+        </div>
       </div>
     </section>
   );
