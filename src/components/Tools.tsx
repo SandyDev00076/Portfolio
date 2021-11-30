f8a66ba5a191e5a215000b144ab362df66f273b2
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-12-01 01:31:18 +0530

    prepare list of techs

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -3,11 +3,37 @@ import Heading from "./Heading";
 
 import styles from "./Tools.module.scss";
 
+const techs = [
+  "React",
+  "TypeScript",
+  "Angular",
+  "Vue",
+  "Ionic",
+  "Cordova",
+  "Redux",
+  "CSS3",
+  "Sass",
+  "NodeJS",
+  "ExpressJS",
+  "AWS Lambda",
+  "Flask",
+  "Figma",
+  "AdobeXD",
+];
+
 const Tools = () => {
   return (
     <section id="tools">
       <Heading dir="right">Tools</Heading>
-      <div className={styles.tools}></div>
+      <div className={styles.tools}>
+        <div className={styles.type}>
+          {techs.map((tech) => (
+            <div className={styles.tech} key={tech}>
+              {tech}
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
